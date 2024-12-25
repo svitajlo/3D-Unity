f4c6cb63d02e3e4d8ee9c4d06ea95cd1976e21bb
svitajlo <svitailo.illia1122@vu.cdu.edu.ua>
2024-12-25 14:02:19 +0200

    3D-Unity

---
+++ Unity_lab-main/Assets/Move.cs
@@ -0,0 +1,24 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public class Move : MonoBehaviour {
+
+    public GameObject goal;
+    Vector3 direction;
+    public float speed = 5f;
+
+    void Start() {
+       // direction = goal.transform.position - this.transform.position;
+    }
+
+    private void LateUpdate() {
+        direction = goal.transform.position - this.transform.position;
+        this.transform.LookAt(goal.transform.position);
+        if (direction.magnitude > 2)
+        {
+        Vector3 velocity = direction.normalized * speed * Time.deltaTime;
+        this.transform.position = this.transform.position + velocity;
+        }
+        
+    }
+}
